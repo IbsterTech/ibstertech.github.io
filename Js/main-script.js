4a5330b8b14e6b6236a635a1173f1ed8e0d661cf
IbsterTech <62631049+IbsterTech@users.noreply.github.com>
2023-11-16 03:36:53 +0000

    WWWW

--- Js/main-script.js
+++ Js/main-script.js
@@ -64,18 +64,76 @@ console.log("A rat race is for rats. We're not rats. We're human beings. Reject 
 console.log("“You can’t win unless you try to win, but you can lose by trying not to lose.”")
 
 // Context Menu Disabling
-// $(document).bind("contextmenu",function(e) { 
-// e.preventDefault();
-// var currentHtmlContent;
+$(document).bind("contextmenu",function(e) { 
+e.preventDefault();
+var currentHtmlContent;
 
-// var element = new Image();
+var element = new Image();
 
-// var elementWithHiddenContent = document.querySelector("#element-to-hide");
+var elementWithHiddenContent = document.querySelector("#element-to-hide");
 
-// var innerHtml = elementWithHiddenContent.innerHTML;
+var innerHtml = elementWithHiddenContent.innerHTML;
 
-// element.__defineGetter__("id", function() {
+element.__defineGetter__("id", function() {
 
-//  currentHtmlContent= "";
+ currentHtmlContent= "";
 
-// })});
+})});
+
+// Typewriter js
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
