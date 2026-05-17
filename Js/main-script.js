7fe951b50c70ee600e45cb79d59db48876a8b856
IbsterTech <62631049+IbsterTech@users.noreply.github.com>
2026-05-17 04:44:30 +0000

    www

--- Js/main-script.js
+++ Js/main-script.js
@@ -62,10 +62,3 @@ $(document).ready(function(){
 // Console Message
 console.log("My unmatched perspicacity, coupled with sheer indefatigability, makes me a feared opponent in any realm of human endeavor.")
 
-
-// Microsoft Clarity 
-(function(c,l,a,r,i,t,y){
-    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-})(window, document, "clarity", "script", "jrs8b0scfr");
